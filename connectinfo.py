58f19d2297d812f2b5958f07879ef14036062463
ssnider5 <ssnider5@outlook.com>
2025-02-26 09:34:17 -0600

    Update connectinfo.py

--- connectinfo.py
+++ connectinfo.py
@@ -1,8 +1,1 @@
-#!/usr/bin/python
 
-class ConnectInfo:
-   
-    def __init__(self):
-        self.host = "qdlp2bcmapp0002.ess.fiserv.one"
-        self.user = "F7JB1KS"
-        self.password = "Mom317digdog!@"
